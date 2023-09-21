bcfd809188015191cf63eb12e25ec969516bc1be
Harshitha Lingala <142623288+Harshitha-Lingala@users.noreply.github.com>
2023-09-21 11:46:08 -0700

    Update stream_fish.py

--- stream_fish.py
+++ stream_fish.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 df=pd.read_csv('Fish.csv')
+
 avg_weight_by_species=df.groupby('Species')['Weight'].mean().reset_index()
-st.write(avg_weight_by_species)
+
 plt.bar(avg_weight_by_species['Species'],avg_weight_by_species['Weight'])
 plt.title('Average Weight of Species')
 plt.xlabel('Species')
