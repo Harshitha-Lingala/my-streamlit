fd97770de9df584ad9c679ffc989051de4f58d04
Harshitha Lingala <142623288+Harshitha-Lingala@users.noreply.github.com>
2023-09-21 11:47:53 -0700

    Update stream_fish.py

--- stream_fish.py
+++ stream_fish.py
@@ -6,7 +6,7 @@ df=pd.read_csv('Fish.csv')
 
 avg_weight_by_species=df.groupby('Species')['Weight'].mean().reset_index()
 
-plt.bar(avg_weight_by_species['Species'],avg_weight_by_species['Weight'],color='green',edgecolor='red')
+plt.bar(avg_weight_by_species['Species'],avg_weight_by_species['Weight'])
 plt.title('Average Weight of Species')
 plt.xlabel('Species')
 plt.ylabel('Avg Weight')
