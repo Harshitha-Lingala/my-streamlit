c6b5936cdefeb27c0d716169db85955cee6ebf0f
Harshitha Lingala <142623288+Harshitha-Lingala@users.noreply.github.com>
2023-09-21 10:22:37 -0700

    Update fish_graph.py

--- fish_graph.py
+++ fish_graph.py
@@ -2,4 +2,4 @@ import streamlit as st
 import pandas as pd
 
 df=pd.read_csv('Fish.csv')
-st.bar_chart(height=df['Species'],x=df['Weight'],width=0.5)
+st.bar_chart(df['Species'],height=df['Weight'],width=0.5)
